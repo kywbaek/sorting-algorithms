eafee9285000624c3f815954b8787af3853766a8
Young Wook Baek <kywbaek@gmail.com>
2018-07-07 03:30:00 +0100

    feat: add example cases to bubble-sort.js

--- bubble-sort/bubble-sort.js
+++ bubble-sort/bubble-sort.js
@@ -15,3 +15,16 @@ function bubbleSort(arr) {
     }
     return arr
 }
+
+let arr1 = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+let arr2 = []
+let arr3 = [3]
+
+// Expected result: [1, 3, 4, 6, 9, 14, 20, 21, 21, 25]
+console.log(bubbleSort(arr1))
+
+// Expected result: []
+console.log(print bubbleSort(arr2))
+
+// Expected result: [3]
+console.log(print bubbleSort(arr3))
